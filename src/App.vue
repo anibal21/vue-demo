599ce681b87d3a842b2998df59400be772afc915
anibal21 <anibalrodriguez1990@gmail.com>
2019-01-09 08:25:39 -0300

    maana

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-      <navigation-guest></navigation-guest>
-      <navigation-logged></navigation-logged>
-    <router-view/>
+    <loading :active.sync="loading"></loading>
+    <b-container>
+      <navigation-logged v-if="isLogged"></navigation-logged>
+      <navigation-guest v-else></navigation-guest>      
+      <router-view/>      
+    </b-container>
   </div>
 </template>
 
 <script>
   import NavigationLogged from '@/components/Navigations/Logged'
   import NavigationGuest from '@/components/Navigations/Guest'
+  import {mapState} from 'vuex'
+  import Loading from 'vue-loading-overlay'
+  import 'vue-loading-overlay/dist/vue-loading.css'
 
   export default {
     components: {
       NavigationLogged,
-      NavigationGuest
+      NavigationGuest,
+      Loading
+    },
+    computed: {
+      ...mapState('auth', ['isLogged']),
+      ...mapState(['loading'])
     }
   }
 </script>
